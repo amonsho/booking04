9e0aa67a30d304fb4bde46fd77a6ecbd79c8aea0
amonsho <amonsho004@gmail.com>
2026-03-12 14:15:41 +0500

    create userRouter

--- app/main.py
+++ app/main.py
@@ -17,5 +17,7 @@ async def init_models():
         await conn.run_sync(Base.metadata.create_all)
 
 from app.api.auth_router import router as auth_router
+from app.api.user_router import router as user_router
 
 app.include_router(auth_router)
+app.include_router(user_router)
